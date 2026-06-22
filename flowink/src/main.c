e7534bb3854519b880cc4027b431631d8e862e3e
nhf_um <2844588775@qq.com>
2026-06-22 23:12:03 +0800

    esp32s3最小构建单元（包含日志和i2s彩灯）

---
+++ flowink/src/main.c
@@ -0,0 +1,14 @@
+#define LOG_LEVEL 4
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main);
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+
+
+int main(void)
+{
+
+    return 0;
+}
+
