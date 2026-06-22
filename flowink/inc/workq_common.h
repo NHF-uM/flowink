53b149383e9bd9d1bfb853270be0b5a40728de41
nhf_um <2844588775@qq.com>
2026-06-22 23:24:36 +0800

    增加工作队列组件

---
+++ flowink/inc/workq_common.h
@@ -0,0 +1,6 @@
+#ifndef _WORKQ_COMMON_H_
+#define _WORKQ_COMMON_H_
+
+void workq_init(void);
+
+#endif /* _WORKQ_COMMON_H_ */
---
+++ flowink/src/workq_common.c
@@ -0,0 +1,15 @@
+#include <zephyr/kernel.h>
+
+#define WORK_QUEUE_STACK_SIZE 512
+#define WORK_QUEUE_PRIORITY 5
+
+K_THREAD_STACK_DEFINE(work_queue_stack, WORK_QUEUE_STACK_SIZE);
+
+struct k_work_q work_queue;
+
+void workq_init(void)
+{
+    k_work_q_init(&work_queue);
+    k_work_queue_start(&work_queue, work_queue_stack, K_THREAD_STACK_SIZEOF(work_queue_stack),
+                       WORK_QUEUE_PRIORITY, NULL);
+}
