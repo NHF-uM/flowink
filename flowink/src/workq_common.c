025a0fb7e8e796061efc54989019ede0b7ee315b
nhf_um <2844588775@qq.com>
2026-06-22 23:34:20 +0800

    fix >工作组件函数名修正

--- flowink/src/workq_common.c
+++ flowink/src/workq_common.c
@@ -9,7 +9,7 @@ struct k_work_q work_queue;
 
 void workq_init(void)
 {
-    k_work_q_init(&work_queue);
+    k_work_queue_init(&work_queue);
     k_work_queue_start(&work_queue, work_queue_stack, K_THREAD_STACK_SIZEOF(work_queue_stack),
                        WORK_QUEUE_PRIORITY, NULL);
 }
