56758462f599a8c614cd910080a57483d84d2edc
user_1784595355177 <email_1784595355177>
2026-07-27 18:51:51 +0800

    初步创建dts和Kconfig

--- flowink/main.c
+++ flowink/main.c
@@ -4,6 +4,18 @@ LOG_MODULE_REGISTER(main);
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
+#include <zephyr/input/input.h>
+#include <ff.h>
+#include <zephyr/fs/fs.h>
+#include <zephyr/storage/disk_access.h>
+
+/*
+ * cp -rfpv ../zephyr/samples/subsys/fs/fs_sample/ . 
+ * cp -rfpv ../zephyr/samples/subsys/fs/fatfs_fstab/ .
+ */
+
+#define AUTOMOUNT_NODE DT_NODELABEL(ffs_sd)
+
 
 int main(void)
 {
