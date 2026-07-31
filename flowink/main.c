f7773b3eb1da54856ca3944671ac29d9e19657bc
nhf_um <2844588775@qq.com>
2026-08-01 00:25:53 +0800

    验证ext1唤醒成功，timer唤醒失败

--- flowink/main.c
+++ flowink/main.c
@@ -4,11 +4,29 @@ LOG_MODULE_REGISTER(main);
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
+#include "pwr_manage.h"
+#include "rgb_strip.h"
 
 int main(void)
 {
-    while (1) {
+    k_sleep(K_SECONDS(30));
+    pwr_set_sleep_timer_wakeup(60);
 
+    uint8_t color = BLUE;
+    if (pwr_get_wakeup_cause() == WAKEUP_TIMER) 
+    {
+        color = RED;
+    } 
+    else 
+    {
+        color = GREEN;
+    }
+    pwr_enter_sleep();
+    while (1) {
+        rgb_strip_on(color);
+        k_sleep(K_MSEC(1000));
+        rgb_strip_off();
+        k_sleep(K_MSEC(1000));
     }
     return 0;
 }
