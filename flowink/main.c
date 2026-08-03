e3685f88e5c2b0d735f27cdbfa37255388928921
nhf_um <2844588775@qq.com>
2026-08-03 23:12:01 +0800

    部分解耦到yaml并成功运行

--- flowink/main.c
+++ flowink/main.c
@@ -16,16 +16,19 @@ LOG_MODULE_REGISTER(main);
 #define PIN_SET(pin) gpio_pin_set(epd_port, pin, 1)
 #define PIN_CLR(pin) gpio_pin_set(epd_port, pin, 0)
 
-#define EPD_7IN3E_WIDTH 800
-#define EPD_7IN3E_HEIGHT 480
+#define EPD_NODE            DT_NODELABEL(epd)
+#define EPD_7IN3E_WIDTH     DT_PROP(EPD_NODE, width)
+#define EPD_7IN3E_HEIGHT    DT_PROP(EPD_NODE, height)
 
-const struct device *epd_port = DEVICE_DT_GET(DT_NODELABEL(gpio0));
 const struct device *epd_spi = DEVICE_DT_GET(DT_NODELABEL(spi2));
-
+// const struct spi_dt_spec epd_spi = SPI_DT_SPEC_GET(EPD_NODE, SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_TRANSFER_MSB, 0);
+const struct gpio_dt_spec epd_gpio_dc = GPIO_DT_SPEC_GET(EPD_NODE, dc_gpios);
+const struct gpio_dt_spec epd_gpio_rst = GPIO_DT_SPEC_GET(EPD_NODE, rst_gpios);
+const struct gpio_dt_spec epd_gpio_busy = GPIO_DT_SPEC_GET(EPD_NODE, busy_gpios);
 
 // 问题：90us的cs拉低，clk有效只占了7us左右
 const struct spi_config epd_spi_cfg = {
-    .frequency = 10000000,
+    .frequency = 1000000,
     .cs = {
         .cs_is_gpio = true,
         .gpio = SPI_CS_GPIOS_DT_SPEC_GET(DT_NODELABEL(epd)),
@@ -41,11 +44,11 @@ const struct spi_config epd_spi_cfg = {
  */
 static void epd_reset(void)
 {
-    PIN_SET(RST_PIN);
+    gpio_pin_set_dt(&epd_gpio_rst, 1);
     k_sleep(K_MSEC(50));
-    PIN_CLR(RST_PIN);
+    gpio_pin_set_dt(&epd_gpio_rst, 0);
     k_sleep(K_MSEC(20));
-    PIN_SET(RST_PIN);
+    gpio_pin_set_dt(&epd_gpio_rst, 1);
     k_sleep(K_MSEC(50));
 }
 
@@ -56,7 +59,7 @@ static void epd_reset(void)
 static void epd_wait_idle(void)
 {
     LOG_INF("e-Paper busy H\r\n");
-    while (!gpio_pin_get(epd_port, BUSY_PIN))
+    while (!gpio_pin_get_dt(&epd_gpio_busy))
     {
         k_sleep(K_MSEC(5));
     }
@@ -65,7 +68,7 @@ static void epd_wait_idle(void)
 
 static void epd_send_command(uint8_t cmd)
 {
-    PIN_CLR(DC_PIN);
+    gpio_pin_set_dt(&epd_gpio_dc, 0);
     struct spi_buf_set tx_buf = {
         .buffers = &(struct spi_buf){.buf = &cmd, .len = 1},
         .count = 1,
@@ -75,7 +78,7 @@ static void epd_send_command(uint8_t cmd)
 
 static void epd_send_data(uint8_t data)
 {
-    PIN_SET(DC_PIN);
+    gpio_pin_set_dt(&epd_gpio_dc, 1);
     struct spi_buf_set tx_buf = {
         .buffers = &(struct spi_buf){.buf = &data, .len = 1},
         .count = 1,
@@ -227,13 +230,37 @@ void epd_fill_image(uint8_t *Image)
 uint8_t color = 0;
 int main(void)
 {
-    gpio_pin_configure(epd_port, DC_PIN, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure(epd_port, RST_PIN, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure(epd_port, BUSY_PIN, GPIO_INPUT | GPIO_PULL_UP);
+    gpio_pin_configure_dt(&epd_gpio_dc, GPIO_OUTPUT_LOW);
+    gpio_pin_configure_dt(&epd_gpio_rst, GPIO_OUTPUT_HIGH);
+    gpio_pin_configure_dt(&epd_gpio_busy, GPIO_INPUT);
+
+    if (!gpio_is_ready_dt(&epd_gpio_dc) || !gpio_is_ready_dt(&epd_gpio_rst) || !gpio_is_ready_dt(&epd_gpio_busy))
+    {
+        LOG_ERR("GPIO device not ready");
+        return 0;
+    }
+
+    // if (!device_is_ready(&epd_spi))
+    // {
+    //     LOG_ERR("SPI device not ready");
+    //     return 0;
+    // }
 
     rgb_strip_on(BLUE);
 
+
     // 先复位再按逻辑分析仪，不然逻辑分析仪会跑飞
+    epd_wait_idle();
+    epd_reset();
+    k_sleep(K_MSEC(25));
+    epd_send_command(0x5a);
+    k_sleep(K_MSEC(25));
+    epd_send_data(0x75);
+    k_sleep(K_MSEC(25));
+    epd_sleep();
+    k_sleep(K_MSEC(25));
+    epd_init();
+
 
     while (1)
     {
