d0139fc9f84aafb748cac057331e3f5c2d84f86e
nhf_um <2844588775@qq.com>
2026-08-03 23:12:01 +0800

    222

--- flowink/main.c
+++ flowink/main.c
@@ -15,12 +15,12 @@ LOG_MODULE_REGISTER(main);
 #define PIN_SET(pin) gpio_pin_set(epd_port, pin, 1)
 #define PIN_CLR(pin) gpio_pin_set(epd_port, pin, 0)
 
-#define EPD_7IN3E_WIDTH       800
-#define EPD_7IN3E_HEIGHT      480
+#define EPD_7IN3E_WIDTH 800
+#define EPD_7IN3E_HEIGHT 480
 
 const struct device *epd_port = DEVICE_DT_GET(DT_NODELABEL(gpio0));
-const struct device *epd_spi = DEVICE_DT_GET(DT_BUS(DT_NODELABEL(epd)));
-// const struct device *epd_spi = DEVICE_DT_GET((DT_NODELABEL(spi2)));
+// const struct device *epd_spi = DEVICE_DT_GET(DT_BUS(DT_NODELABEL(epd)));
+const struct device *epd_spi = DEVICE_DT_GET(DT_NODELABEL(spi2));
 
 // #define EPD_SPI_FLASS (SPI_OP_MODE_MASTER | )
 // const struct spi_dt_spec epd_spi_spec = SPI_DT_SPEC_GET(DT_NODELABEL(epd), 0);
@@ -28,14 +28,18 @@ const struct device *epd_spi = DEVICE_DT_GET(DT_BUS(DT_NODELABEL(epd)));
 const struct spi_config epd_spi_cfg = {
     .frequency = 1125000,
     .cs = {
-	.cs_is_gpio = true,
-	.gpio = SPI_CS_GPIOS_DT_SPEC_GET(DT_NODELABEL(epd)),
-	.delay = 0,
+        .cs_is_gpio = true,
+        .gpio = SPI_CS_GPIOS_DT_SPEC_GET(DT_NODELABEL(epd)),
+        .delay = 0,
     },
     .operation = SPI_WORD_SET(8) | SPI_OP_MODE_MASTER,
     .slave = 0,
 };
 
+/**
+ * @brief 复位屏幕驱动芯片
+ * @param  无
+ */
 static void epd_reset(void)
 {
     PIN_SET(RST_PIN);
@@ -46,13 +50,17 @@ static void epd_reset(void)
     k_sleep(K_MSEC(20));
 }
 
+/**
+ * @brief 等待BUSY引脚拉高
+ * @param  无
+ */
 static void epd_wait_idle(void)
 {
     LOG_INF("e-Paper busy H\r\n");
     while (!gpio_pin_get(epd_port, BUSY_PIN))
     {
         k_sleep(K_MSEC(1));
-    } 
+    }
     LOG_INF("e-Paper busy H release\r\n");
 }
 
@@ -60,8 +68,8 @@ static void epd_send_command(uint8_t cmd)
 {
     PIN_CLR(DC_PIN);
     struct spi_buf_set tx_buf = {
-	.buffers = &(struct spi_buf){ .buf = &cmd, .len = 1 },
-	.count = 1,
+        .buffers = &(struct spi_buf){.buf = &cmd, .len = 1},
+        .count = 1,
     };
     spi_write(epd_spi, &epd_spi_cfg, &tx_buf);
 }
@@ -70,8 +78,8 @@ static void epd_send_data(uint8_t data)
 {
     PIN_SET(DC_PIN);
     struct spi_buf_set tx_buf = {
-	.buffers = &(struct spi_buf){ .buf = &data, .len = 1 },
-	.count = 1,
+        .buffers = &(struct spi_buf){.buf = &data, .len = 1},
+        .count = 1,
     };
     spi_write(epd_spi, &epd_spi_cfg, &tx_buf);
 }
@@ -82,7 +90,7 @@ void epd_init(void)
     epd_wait_idle();
     k_sleep(K_MSEC(30));
 
-    epd_send_command(0xAA);    // CMDH
+    epd_send_command(0xAA);
     epd_send_data(0x49);
     epd_send_data(0x55);
     epd_send_data(0x20);
@@ -90,10 +98,10 @@ void epd_init(void)
     epd_send_data(0x09);
     epd_send_data(0x18);
 
-    epd_send_command(0x01);//
+    epd_send_command(0x01);
     epd_send_data(0x3F);
 
-    epd_send_command(0x00);  
+    epd_send_command(0x00);
     epd_send_data(0x5F);
     epd_send_data(0x69);
 
@@ -101,7 +109,7 @@ void epd_init(void)
     epd_send_data(0x00);
     epd_send_data(0x54);
     epd_send_data(0x00);
-    epd_send_data(0x44); 
+    epd_send_data(0x44);
 
     epd_send_command(0x05);
     epd_send_data(0x40);
@@ -123,7 +131,7 @@ void epd_init(void)
 
     epd_send_command(0x30);
     epd_send_data(0x03);
-    
+
     epd_send_command(0x50);
     epd_send_data(0x3F);
 
@@ -143,16 +151,21 @@ void epd_init(void)
     epd_send_command(0xE3);
     epd_send_data(0x2F);
 
-    epd_send_command(0x04);     //PWR on  
-    epd_wait_idle();          //waiting for the electronic paper IC to release the idle signal
+    /* 打开电源并且等待稳定 */
+    epd_send_command(0x04);
+    epd_wait_idle();
 }
 
+/**
+ * @brief 开电源-刷新（包含等待）-关电源
+ * @param  无
+ */
 static void epd_refresh(void)
 {
     epd_send_command(0x04); // POWER_ON
     epd_wait_idle();
 
-    //Second setting 
+    // Second setting
     epd_send_command(0x06);
     epd_send_data(0x6F);
     epd_send_data(0x1F);
@@ -162,7 +175,7 @@ static void epd_refresh(void)
     epd_send_command(0x12); // DISPLAY_REFRESH
     epd_send_data(0x00);
     epd_wait_idle();
-    
+
     epd_send_command(0x02); // POWER_OFF
     epd_send_data(0X00);
     epd_wait_idle();
@@ -181,15 +194,15 @@ void epd_sleep(void)
 void epd_fill_color(uint8_t color)
 {
     uint16_t Width, Height;
-    Width = (EPD_7IN3E_WIDTH % 2 == 0)? (EPD_7IN3E_WIDTH / 2 ): (EPD_7IN3E_WIDTH / 2 + 1);
+    Width = (EPD_7IN3E_WIDTH % 2 == 0) ? (EPD_7IN3E_WIDTH / 2) : (EPD_7IN3E_WIDTH / 2 + 1);
     Height = EPD_7IN3E_HEIGHT;
 
     epd_send_command(0x10);
-    for (uint16_t j = 0; j < Height; j++) 
+    for (uint16_t j = 0; j < Height; j++)
     {
-        for (uint16_t i = 0; i < Width; i++) 
-	{
-            epd_send_data((color<<4)|color);
+        for (uint16_t i = 0; i < Width; i++)
+        {
+            epd_send_data((color << 4) | color);
         }
     }
 
@@ -199,12 +212,14 @@ void epd_fill_color(uint8_t color)
 void epd_fill_image(uint8_t *Image)
 {
     uint16_t Width, Height;
-    Width = (EPD_7IN3E_WIDTH % 2 == 0)? (EPD_7IN3E_WIDTH / 2 ): (EPD_7IN3E_WIDTH / 2 + 1);
+    Width = (EPD_7IN3E_WIDTH % 2 == 0) ? (EPD_7IN3E_WIDTH / 2) : (EPD_7IN3E_WIDTH / 2 + 1);
     Height = EPD_7IN3E_HEIGHT;
 
     epd_send_command(0x10);
-    for (uint16_t j = 0; j < Height; j++) {
-        for (uint16_t i = 0; i < Width; i++) {
+    for (uint16_t j = 0; j < Height; j++)
+    {
+        for (uint16_t i = 0; i < Width; i++)
+        {
             epd_send_data(Image[i + j * Width]);
         }
     }
@@ -215,14 +230,21 @@ int main(void)
 {
     gpio_pin_configure(epd_port, DC_PIN, GPIO_OUTPUT_INACTIVE);
     gpio_pin_configure(epd_port, RST_PIN, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure(epd_port, BUSY_PIN, GPIO_OUTPUT_INACTIVE);
-
+    gpio_pin_configure(epd_port, BUSY_PIN, GPIO_INPUT);
 
     rgb_strip_on(BLUE);
-    
-    while (1) 
+
+    while (1)
     {
-        
+        epd_reset();
+        k_sleep(K_MSEC(50));
+        epd_send_command(0x5a);
+        k_sleep(K_MSEC(50));
+        epd_send_data(0x75);
+        k_sleep(K_MSEC(50));
+        epd_wait_idle();
+        k_sleep(K_MSEC(50));
+        epd_sleep();
     }
     return 0;
 }
