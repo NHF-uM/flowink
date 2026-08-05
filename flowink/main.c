c06078a9e0842a4a17ef23d0f2e9a8ef26733e07
uM_UMber <2844588775@qq.com>
2026-08-05 12:34:27 +0800

    增加闪灯代码；测试出来可以开启ap模式，但是会卡死

--- flowink/main.c
+++ flowink/main.c
@@ -6,6 +6,7 @@ LOG_MODULE_REGISTER(main);
 #include <zephyr/device.h>
 #include <zephyr/net/wifi_mgmt.h>
 #include <zephyr/net/dhcpv4_server.h>
+#include "rgb_strip.h"
 
 #define MACSTR "%02X:%02X:%02X:%02X:%02X:%02X"
 
@@ -151,22 +152,63 @@ int main(void)
 
     while (1)
     {
+        rgb_strip_on(BLUE);
+        k_sleep(K_SECONDS(1));
+        rgb_strip_off();
+        k_sleep(K_SECONDS(1));
     }
     return 0;
 }
 
-// static const uint16_t http_service_port = CONFIG_NET_HTTP_SERVER_PORT;
-
 /*
-6. _detail 用户私有自定义数据
-任意指针，会挂载在 http_service 结构体里。
-业务场景：
-传入设备配置结构体、状态指针；
-在路由回调函数中通过 service->detail 拿到业务上下文，不用全局变量。
-7. _res_fallback 兜底资源（404 页面）
-当访问的 URL 没有匹配任何已注册路由时，就返回这个资源。
-一般用 HTTP_RESOURCE_STATIC() 定义一个返回 404 html 的静态资源。
-*/
 
-// HTTP_SERVICE_DEFINE(http_service, NULL, &http_service_port,
-// 		    CONFIG_HTTP_SERVER_MAX_CLIENTS, 10, NULL, NULL, NULL);
+连接一次之后就卡死了，ap热点也找不到了：
+
+Build:Mar 27 2021
+rst:0x15 (USB_UART_CHIP_RESET),boot:0x8 (SPI_FAST_FLASH_BOOT)
+Saved PC:0x40378eba
+SPIWP:0xee
+mode:DIO, clock div:1
+load:0x3fc91f30,len:0x5324
+load:0x40374000,len:0xdf20
+load:0x50000000,len:0x24
+SHA-256 comparison failed:
+Calculated: 0b898f4b5f7721db68f9b71429d06b68849758634e72c93667dd9095aeac9ed0
+Expected: 0000000050cd0000000000000000000000000000000000000000000000000000
+Attempting to boot anyway...
+entry 0x4037c32c
+I (soc_init): ESP Simple boot
+I (soc_init): compile time Aug  5 2026 12:27:32
+W (soc_init): Unicore bootloader
+I (soc_init): chip revision: v0.2
+I (flash_init): Boot SPI Speed : 80MHz
+I (flash_init): SPI Mode       : DIO
+I (flash_init): SPI Flash Size : 16MB
+I (boot): DRAM	: lma=00000020h vma=3fc91f30h size=05324h ( 21284)
+I (boot): IRAM	: lma=0000534ch vma=40374000h size=0df20h ( 57120)
+I (boot): RTC_DATA	: lma=00013274h vma=50000000h size=00024h (    36)
+I (boot): IROM	: lma=00020000h vma=42000000h size=5ecb8h (388280)
+I (boot): DROM	: lma=00080000h vma=3c060000h size=0da28h ( 55848)
+I (boot): libc heap size 200 kB.
+I (cache): Instruction cache: size 16KB, 8Ways, cache line size 32Byte
+I (spi_flash): detected chip: boya
+I (spi_flash): flash io: dio
+I (pp): pp rom version: e7ae62f
+I (net80211): net80211 rom version: e7ae62f
+I (wifi_init): rx ba win: 6
+*** Booting Zephyr OS build v4.4.0-645-ga0372ca149a6 ***
+[00:00:05.087,000] <inf> main: Turning on AP Mode
+[00:00:05.088,000] <dbg> net_dhcpv4_server: net_dhcpv4_server_start: Started DHCPv4 server, address pool:
+[00:00:05.088,000] <dbg> net_dhcpv4_server: net_dhcpv4_server_start: 	 0: 192.168.4.11
+[00:00:05.088,000] <dbg> net_dhcpv4_server: net_dhcpv4_server_start: 	 1: 192.168.4.12
+[00:00:05.088,000] <dbg> net_dhcpv4_server: net_dhcpv4_server_start: 	 2: 192.168.4.13
+[00:00:05.088,000] <dbg> net_dhcpv4_server: net_dhcpv4_server_start: 	 3: 192.168.4.14
+[00:00:05.088,000] <inf> main: DHCPv4 server started...
+
+[00:00:05.696,000] <wrn> net_if: iface 2 pkt 0x3fcaf144 send failure status -5
+[00:00:05.697,000] <inf> main: AP Mode is enabled. Waiting for station to connect
+[00:01:36.998,000] <inf> main: station: 42:DA:1A:EF:18:A8 joined 
+[00:01:37.161,000] <dbg> net_dhcpv4_server: dhcpv4_handle_discover: DHCPv4 processing Discover - reserved 192.168.4.11
+
+
+*/
