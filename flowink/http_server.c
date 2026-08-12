bfc1999c49cb2c830f128bbc0ef9731550ab1af8
uM_UMber <15297657+umumber@user.noreply.gitee.com>
2026-08-12 15:26:49 +0800

    完删http功能，剩余和刷图结合了（未测试）

--- flowink/http_server.c
+++ flowink/http_server.c
@@ -51,57 +51,59 @@ static struct http_resource_detail_static zkk_png_resource_detail = {
 	.static_data_len = sizeof(zkk_png),
 };
 
-// static size_t data_received;
+static size_t data_received;
 
-// static int data_up_handler(struct http_client_ctx *client, enum http_transaction_status status,
-// 						   const struct http_request_ctx *request_ctx,
-// 						   struct http_response_ctx *response_ctx, void *user_data)
-// {
-// 	static size_t data_received;
+static int data_up_handler(struct http_client_ctx *client, enum http_transaction_status status,
+						   const struct http_request_ctx *request_ctx,
+						   struct http_response_ctx *response_ctx, void *user_data)
+{
+	switch (status)
+	{
+	case HTTP_SERVER_TRANSACTION_ABORTED:
+	{
+		LOG_DBG("Transaction aborted after %zd bytes.", data_received);
+		data_received = 0;
+		return -1;
+	}
+	case HTTP_SERVER_TRANSACTION_COMPLETE:	/* 响应也发送完成 */
+	{
+		LOG_DBG("Transmission completed, including response");
+		data_received = 0;
+		return 0;
+	}
+	default:
+		break;
+	}
 
-// 	switch (status)
-// 	{
-// 	case HTTP_SERVER_TRANSACTION_ABORTED:
-// 	{
-// 		LOG_DBG("Transaction aborted after %zd bytes.", data_received);
-// 		data_received = 0;
-// 		return -1;
-// 	}
-// 	case HTTP_SERVER_TRANSACTION_COMPLETE:
-// 	{
-// 		data_received = 0;
-// 		break;
-// 	}
-// 	default:
-// 		break;
-// 	}
+	/* 未发送完成，存储数据*/
+	data_received += request_ctx->data_len;
+	LOG_DBG("data has been received (%zd bytes)", data_received);
 
-// 	/* 未发送完成，存储数据*/
-// 	data_received += request_ctx->data_len;
+	if (status == HTTP_SERVER_REQUEST_DATA_FINAL)
+	{
+		LOG_DBG("picture received succ (%zd bytes).", data_received);
+		data_received = 0;
 
-// 	if (status == HTTP_SERVER_REQUEST_DATA_FINAL)
-// 	{
-// 		LOG_DBG("bmp picture received (%zd bytes).", processed);
-// 		processed = 0;
+		// 传输并保存成功if ()
+		static char *response_str = "Upload Succ";
+		/* 填充response，status 和 header只会在第一次回调的时候被填充 */
+		response_ctx->status = HTTP_200_OK;
+		response_ctx->body = (const uint8_t*)response_str;
+		response_ctx->body_len = sizeof(response_str) - 1;
+		/* 只有当 final_chunk 为1的时候，才会发送response（结构和request相似，一次response只对应一次request，分包只是 TCP 的作用，和http没有关系） */
+		response_ctx->final_chunk = true;
+	}
+	return 0;
+}
 
-// 		// 传输并保存成功if ()
-// 		char *response_str = "Upload Succ";
-// 		/* 填充response */
-
-// 		/* 只有当 final_chunk 为1的时候，才会发送response（结构和request相似，一次response只对应一次request，分包只是 TCP 的作用，和http没有关系） */
-// 		response_ctx->final_chunk = true;
-// 	}
-// 	return 0;
-// }
-
-// static struct http_resource_detail_dynamic data_up_resource_detail = {
-// 	.common = {
-// 		.type = HTTP_RESOURCE_TYPE_DYNAMIC,
-// 		.bitmask_of_supported_http_methods = BIT(HTTP_POST),
-// 	},
-// 	.cb = data_up_handler,
-// 	.user_data = NULL,
-// };
+static struct http_resource_detail_dynamic data_up_resource_detail = {
+	.common = {
+		.type = HTTP_RESOURCE_TYPE_DYNAMIC,
+		.bitmask_of_supported_http_methods = BIT(HTTP_POST),
+	},
+	.cb = data_up_handler,
+	.user_data = NULL,
+};
 
 static uint16_t http_service_port = CONFIG_HTTP_SERVER_SERVICE_PORT;
 HTTP_SERVICE_DEFINE(http_service, NULL, &http_service_port,
@@ -112,4 +114,4 @@ HTTP_RESOURCE_DEFINE(index_html_gz_resource, http_service, "/",
 HTTP_RESOURCE_DEFINE(script_js_gz_resource, http_service, "/script.js",
 					 &script_js_gz_resource_detail);
 HTTP_RESOURCE_DEFINE(zkk_png_resource, http_service, "/zkk.png", &zkk_png_resource_detail);
-// HTTP_RESOURCE_DEFINE(data_up_resource, http_service, "/dataUP", &data_up_resource_detail);
+HTTP_RESOURCE_DEFINE(data_up_resource, http_service, "/dataUP", &data_up_resource_detail);
