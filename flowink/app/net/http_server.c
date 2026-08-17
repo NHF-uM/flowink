c1d3258ac8a0181ba5a02d800891a47119182ab0
uM_UMber <15297657+umumber@user.noreply.gitee.com>
2026-08-17 15:02:47 +0800

    融合！！net

---
+++ flowink/app/net/http_server.c
@@ -0,0 +1,123 @@
+#include <zephyr/net/http/server.h>
+#include <zephyr/net/http/service.h>
+#include <zephyr/net/net_ip.h>
+#include <zephyr/net/socket.h>
+#include <zephyr/sys/util_macro.h>
+#include <zephyr/net/net_config.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(http_server, LOG_LEVEL_DBG);
+
+static const uint8_t index_html_gz[] = {
+#include "index.html.gz.inc"
+};
+
+static const uint8_t script_js_gz[] = {
+#include "script.js.gz.inc"
+};
+
+static const uint8_t zkk_png[] = {
+#include "zkk.png.inc"
+};
+
+static struct http_resource_detail_static index_html_gz_resource_detail = {
+	.common = {
+		.type = HTTP_RESOURCE_TYPE_STATIC,
+		.bitmask_of_supported_http_methods = BIT(HTTP_GET),
+		.content_encoding = "gzip",
+		.content_type = "text/html",
+	},
+	.static_data = index_html_gz,
+	.static_data_len = sizeof(index_html_gz),
+};
+
+static struct http_resource_detail_static script_js_gz_resource_detail = {
+	.common = {
+		.type = HTTP_RESOURCE_TYPE_STATIC,
+		.bitmask_of_supported_http_methods = BIT(HTTP_GET),
+		.content_encoding = "gzip",
+		.content_type = "text/javascriptt",
+	},
+	.static_data = script_js_gz,
+	.static_data_len = sizeof(script_js_gz),
+};
+
+static struct http_resource_detail_static zkk_png_resource_detail = {
+	.common = {
+		.type = HTTP_RESOURCE_TYPE_STATIC,
+		.bitmask_of_supported_http_methods = BIT(HTTP_GET),
+		.content_type = "image/png",
+	},
+	.static_data = zkk_png,
+	.static_data_len = sizeof(zkk_png),
+};
+
+static size_t data_received;
+
+static int data_up_handler(struct http_client_ctx *client, enum http_transaction_status status,
+						   const struct http_request_ctx *request_ctx,
+						   struct http_response_ctx *response_ctx, void *user_data)
+{
+	switch (status)
+	{
+	case HTTP_SERVER_TRANSACTION_ABORTED:
+	{
+		LOG_DBG("Transaction aborted after %zd bytes.", data_received);
+		data_received = 0;
+		return -1;
+	}
+	case HTTP_SERVER_TRANSACTION_COMPLETE: /* 响应也发送完成 */
+	{
+		LOG_DBG("Transmission completed, including response");
+		data_received = 0;
+		return 0;
+	}
+	default:
+		break;
+	}
+
+	/* 未发送完成，存储数据*/
+	data_received += request_ctx->data_len;
+	
+	/* 每隔8192字节发送一次日志（太快的话log会丢包） */
+	if (data_received / 8192 > (data_received - request_ctx->data_len) / 8192)
+	{
+		LOG_DBG("data has been received (%zd bytes)", data_received);
+	}
+
+	if (status == HTTP_SERVER_REQUEST_DATA_FINAL)
+	{
+		LOG_DBG("picture received succ (%zd bytes).", data_received);
+		data_received = 0;
+
+		// 传输并保存成功if ()
+		static char *response_str = "Upload Succ";
+		/* 填充response，status 和 header只会在第一次回调的时候被填充 */
+		response_ctx->status = HTTP_200_OK;
+		response_ctx->body = (const uint8_t *)response_str;
+		response_ctx->body_len = sizeof(response_str) - 1;
+		/* 只有当 final_chunk 为1的时候，才会发送response（结构和request相似，一次response只对应一次request，分包只是 TCP 的作用，和http没有关系） */
+		response_ctx->final_chunk = true;
+	}
+	return 0;
+}
+
+static struct http_resource_detail_dynamic data_up_resource_detail = {
+	.common = {
+		.type = HTTP_RESOURCE_TYPE_DYNAMIC,
+		.bitmask_of_supported_http_methods = BIT(HTTP_POST),
+	},
+	.cb = data_up_handler,
+	.user_data = NULL,
+};
+
+static uint16_t http_service_port = CONFIG_HTTP_SERVER_SERVICE_PORT;
+HTTP_SERVICE_DEFINE(http_service, NULL, &http_service_port,
+					CONFIG_HTTP_SERVER_MAX_CLIENTS, 10, NULL, NULL, NULL);
+
+HTTP_RESOURCE_DEFINE(index_html_gz_resource, http_service, "/",
+					 &index_html_gz_resource_detail);
+HTTP_RESOURCE_DEFINE(script_js_gz_resource, http_service, "/script.js",
+					 &script_js_gz_resource_detail);
+HTTP_RESOURCE_DEFINE(zkk_png_resource, http_service, "/zkk.png", &zkk_png_resource_detail);
+HTTP_RESOURCE_DEFINE(data_up_resource, http_service, "/dataUP", &data_up_resource_detail);
---
+++ flowink/app/net/wifi_ap_dhcp.c
@@ -0,0 +1,144 @@
+#include <zephyr/kernel.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/dhcpv4_server.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(wifi_ap_dhcp, LOG_LEVEL_DBG);
+
+#define STR_TO_MAC "%02X:%02X:%02X:%02X:%02X:%02X"
+
+#define NET_EVENT_WIFI_MASK                                               \
+    (NET_EVENT_WIFI_AP_ENABLE_RESULT | NET_EVENT_WIFI_AP_DISABLE_RESULT | \
+     NET_EVENT_WIFI_AP_STA_CONNECTED | NET_EVENT_WIFI_AP_STA_DISCONNECTED)
+
+static struct net_if *ap_iface;
+
+static struct net_mgmt_event_callback net_mgmt_cb;
+
+static void wifi_event_handler(struct net_mgmt_event_callback *cb, uint64_t mgmt_event,
+                               struct net_if *iface)
+{
+    switch (mgmt_event)
+    {
+    case NET_EVENT_WIFI_AP_ENABLE_RESULT:
+    {
+        LOG_INF("AP Mode is enabled.");
+        break;
+    }
+    case NET_EVENT_WIFI_AP_DISABLE_RESULT:
+    {
+        LOG_INF("AP Mode is disabled.");
+        break;
+    }
+    case NET_EVENT_WIFI_AP_STA_CONNECTED:
+    {
+        struct wifi_ap_sta_info *sta_info = (struct wifi_ap_sta_info *)cb->info;
+
+        LOG_DBG("station: " STR_TO_MAC " joined ", sta_info->mac[0], sta_info->mac[1],
+                sta_info->mac[2], sta_info->mac[3], sta_info->mac[4], sta_info->mac[5]);
+        break;
+    }
+    case NET_EVENT_WIFI_AP_STA_DISCONNECTED:
+    {
+        struct wifi_ap_sta_info *sta_info = (struct wifi_ap_sta_info *)cb->info;
+
+        LOG_DBG("station: " STR_TO_MAC " leave ", sta_info->mac[0], sta_info->mac[1],
+                sta_info->mac[2], sta_info->mac[3], sta_info->mac[4], sta_info->mac[5]);
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+/// @brief 设置完成本机地址和掩码才能打开 dhcp
+/// @param 无
+static void enable_dhcpv4_server(void)
+{
+    static struct net_in_addr addr;
+    static struct net_in_addr netmaskAddr;
+
+    /* 字符串转地址 */
+    if (net_addr_pton(NET_AF_INET, CONFIG_WIFI_AP_IP_ADDRESS, &addr))
+    {
+        LOG_ERR("Invalid address: %s", CONFIG_WIFI_AP_IP_ADDRESS);
+        return;
+    }
+
+    if (net_addr_pton(NET_AF_INET, CONFIG_WIFI_AP_NETMASK, &netmaskAddr))
+    {
+        LOG_ERR("Invalid netmask: %s", CONFIG_WIFI_AP_NETMASK);
+        return;
+    }
+
+    /* 为网络接口设置 IPv4 网关，在 ap 模式下不需要设置 */
+    // net_if_ipv4_set_gw(ap_iface, &addr);
+
+    if (net_if_ipv4_addr_add(ap_iface, &addr, NET_ADDR_MANUAL, 0) == NULL)
+    {
+        LOG_ERR("unable to set IP address for AP interface");
+    }
+
+    if (!net_if_ipv4_set_netmask_by_addr(ap_iface, &addr, &netmaskAddr))
+    {
+        LOG_ERR("Unable to set netmask for AP interface: %s",
+                CONFIG_WIFI_AP_NETMASK);
+    }
+
+    /* 从 本机ip + 1 开始分配 */
+    addr.s4_addr[3] += 1;
+
+    if (net_dhcpv4_server_start(ap_iface, &addr) != 0)
+    {
+        LOG_ERR("DHCP server is not started for desired IP");
+        return;
+    }
+
+    LOG_INF("DHCPv4 server started...\n");
+}
+
+static int enable_ap_mode(void)
+{
+    struct wifi_connect_req_params ap_config = {0};
+
+    ap_config.ssid = (const uint8_t *)CONFIG_WIFI_AP_SSID;
+    ap_config.ssid_length = sizeof(CONFIG_WIFI_AP_SSID) - 1;
+    ap_config.psk = (const uint8_t *)CONFIG_WIFI_AP_PSK;
+    ap_config.psk_length = sizeof(CONFIG_WIFI_AP_PSK) - 1;
+    ap_config.channel = WIFI_CHANNEL_ANY;
+    ap_config.band = WIFI_FREQ_BAND_2_4_GHZ;
+
+    if (sizeof(CONFIG_WIFI_AP_PSK) == 1)
+    {
+        ap_config.security = WIFI_SECURITY_TYPE_NONE;
+    }
+    else
+    {
+        ap_config.security = WIFI_SECURITY_TYPE_PSK;
+    }
+
+    int ret = net_mgmt(NET_REQUEST_WIFI_AP_ENABLE, ap_iface, &ap_config,
+                       sizeof(struct wifi_connect_req_params));
+    if (ret)
+    {
+        LOG_ERR("NET_REQUEST_WIFI_AP_ENABLE failed, err: %d", ret);
+    }
+
+    return ret;
+}
+
+void wifi_init(void)
+{
+    net_mgmt_init_event_callback(&net_mgmt_cb, wifi_event_handler, NET_EVENT_WIFI_MASK);
+    net_mgmt_add_event_callback(&net_mgmt_cb);
+
+    ap_iface = net_if_get_wifi_sap();
+    if (!ap_iface)
+    {
+        LOG_ERR("AP interface not found");
+        return;
+    }
+
+    enable_dhcpv4_server();
+    enable_ap_mode();
+}
