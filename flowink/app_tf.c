5ec6da8cd88713d2818332cde79a441c654c6c60
uM_UMber <15297657+umumber@user.noreply.gitee.com>
2026-08-20 11:37:25 +0800

    sd卡的基本读取代码（未测试）；增加app_tf.c

---
+++ flowink/app_tf.c
@@ -0,0 +1,27 @@
+
+// char *file_tf;
+// char *file_memory;
+
+// struct carousel_info
+// {
+//     char *carousel_file_path;
+//     uint32_t carousel_interval;
+//     bool auto_play;
+// }
+
+// voidtf_ls_file(void)
+// {
+// }
+
+// const char *tf_get_next_file(const char *file_current)
+// {
+//     sys_dlist_peek_head
+// }
+
+// void tf_read_config(struct carousel_info *carousel_info)
+// {
+// }
+
+// void tf_read_bmp(const char *file_path, uint8_t *const bmp_buf)
+// {
+// }
